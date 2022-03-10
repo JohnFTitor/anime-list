8632fe99564af35c1ee69088f6f4f8192f642520
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-03-10 17:00:13 -0500

    Style and extend details page

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -4,6 +4,7 @@ import { useDispatch } from 'react-redux';
 import { v4 as uuid } from 'uuid';
 import { getAnimeById } from '../redux/util/APIHandling';
 import { changePage } from '../redux/pageDetails/pageDetails';
+import '../styles/detailspage.scss';
 
 const DetailsPage = () => {
   const dispatch = useDispatch();
@@ -19,53 +20,56 @@ const DetailsPage = () => {
     (anime.title ? (
       <div className="anime-details">
         <img src={anime.images.webp.large_image_url} alt={anime.title} />
-        <div>
+        <div className="anime-description">
           <h2>{anime.title}</h2>
           <h3>{anime.title_japanese}</h3>
         </div>
-        <ul>
+        <ul className="row genres">
           {anime.genres.map((genre) => (
-            <li key={uuid()}>{genre.name}</li>
+            <li key={uuid()} className="box genre-box">{genre.name}</li>
           ))}
         </ul>
+        <h3 className="title"> Airing Information </h3>
         <div>
-          <p>
-            Year:
-            {anime.year}
+          <p className="box details-box">
+            {`Year: ${anime.year ? anime.year : 'Not Specified'}`}
           </p>
-          <p>
-            Episodes:
-            {anime.episodes}
+          <p className="box details-box">
+            {`Episodes: ${anime.episodes ? anime.episodes : 'Not Specified'}`}
           </p>
-          <p>
-            Duration:
-            {anime.duration}
+          <p className="box details-box">
+            {`Duration: ${anime.duration ? anime.duration : 'Not Specified'}`}
           </p>
-          <p>{anime.status}</p>
+          <p className="box details-box">{`Status: ${anime.status ? anime.status : 'Not Specified'}`}</p>
         </div>
+        <h3 className="title"> More Information </h3>
         <div>
-          <p>{anime.rating}</p>
-          <p>{anime.score}</p>
-          <p>{anime.season}</p>
+          <p>{`Rating: ${anime.rating ? anime.rating : 'Not Specified'}`}</p>
+          <p>{`Score: ${anime.score}`}</p>
+          <p>{`Season: ${anime.season ? anime.season : 'Not Specified'}`}</p>
         </div>
-        <p>{anime.synopsis}</p>
-        {(anime.trailer.embed_url && (
-        <iframe
-          src={anime.trailer.embed_url}
-          title="YouTube video player"
-          frameBorder="0"
-          allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-          allowFullScreen
-        />
-        ))}
-        <ul>
+        <h3 className="title"> Synopsis </h3>
+        <p className="synopsis">{anime.synopsis}</p>
+        <h3 className="title"> Trailer </h3>
+        {anime.trailer.embed_url ? (
+          <iframe
+            src={anime.trailer.embed_url}
+            title="YouTube video player"
+            frameBorder="0"
+            allow="accelerometer; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+            allowFullScreen
+          />
+        ) : <p>{'We couldn\'t find any link. Sorry :('}</p>}
+        <h3 className="title"> Producers </h3>
+        <ul className="row genres">
           {anime.producers.map((producer) => (
-            <li key={uuid()}>{producer.name}</li>
+            <li key={uuid()} className="box">{producer.name}</li>
           ))}
         </ul>
-        <ul>
+        <h3 className="title"> Studios </h3>
+        <ul className="row genres">
           {anime.studios.map((studio) => (
-            <li key={uuid()}>{studio.name}</li>
+            <li key={uuid()} className="box">{studio.name}</li>
           ))}
         </ul>
       </div>
