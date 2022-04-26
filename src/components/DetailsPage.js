009589d53209c295f962987da097b6480fae71d9
Andrés Felipe Arroyave Naranjo <andresf.2117@gmail.com>
2022-04-26 18:00:38 -0500

    Add rearrange media query

--- src/components/DetailsPage.js
+++ src/components/DetailsPage.js
@@ -44,39 +44,47 @@ const DetailsPage = () => {
             allowFullScreen
           />
         ) : <p>{'We couldn\'t find any link. Sorry :('}</p>}
-        <h3 className="title"> Airing Information </h3>
-        <div>
-          <p className="box details-box">
-            {`Year: ${anime.year ? anime.year : 'Not Specified'}`}
-          </p>
-          <p className="box details-box">
-            {`Episodes: ${anime.episodes ? anime.episodes : 'Not Specified'}`}
-          </p>
-          <p className="box details-box">
-            {`Duration: ${anime.duration ? anime.duration : 'Not Specified'}`}
-          </p>
-          <p className="box details-box">{`Status: ${anime.status ? anime.status : 'Not Specified'}`}</p>
+        <div className="stats">
+          <div className="airing">
+            <h3 className="title"> Airing Information </h3>
+            <p className="box details-box">
+              {`Year: ${anime.year ? anime.year : 'Not Specified'}`}
+            </p>
+            <p className="box details-box">
+              {`Episodes: ${anime.episodes ? anime.episodes : 'Not Specified'}`}
+            </p>
+            <p className="box details-box">
+              {`Duration: ${anime.duration ? anime.duration : 'Not Specified'}`}
+            </p>
+            <p className="box details-box">{`Status: ${anime.status ? anime.status : 'Not Specified'}`}</p>
+          </div>
+          <div className="synopsis">
+            <h3 className="title"> Synopsis </h3>
+            <p className="synopsis">{anime.synopsis}</p>
+          </div>
+          <div className="more-info">
+            <h3 className="title"> More Information </h3>
+            <p className="box details-box">{`Rating: ${anime.rating ? anime.rating : 'Not Specified'}`}</p>
+            <p className="box details-box">{`Score: ${anime.score}`}</p>
+            <p className="box details-box">{`Season: ${anime.season ? anime.season : 'Not Specified'}`}</p>
+          </div>
+          <div className="producers">
+            <h3 className="title"> Producers </h3>
+            <ul className="row genres">
+              {anime.producers.map((producer) => (
+                <li key={uuid()} className="box">{producer.name}</li>
+              ))}
+            </ul>
+          </div>
+          <div className="studios">
+            <h3 className="title"> Studios </h3>
+            <ul className="row genres">
+              {anime.studios.map((studio) => (
+                <li key={uuid()} className="box">{studio.name}</li>
+              ))}
+            </ul>
+          </div>
         </div>
-        <h3 className="title"> Synopsis </h3>
-        <p className="synopsis">{anime.synopsis}</p>
-        <h3 className="title"> More Information </h3>
-        <div>
-          <p className="box details-box">{`Rating: ${anime.rating ? anime.rating : 'Not Specified'}`}</p>
-          <p className="box details-box">{`Score: ${anime.score}`}</p>
-          <p className="box details-box">{`Season: ${anime.season ? anime.season : 'Not Specified'}`}</p>
-        </div>
-        <h3 className="title"> Producers </h3>
-        <ul className="row genres">
-          {anime.producers.map((producer) => (
-            <li key={uuid()} className="box">{producer.name}</li>
-          ))}
-        </ul>
-        <h3 className="title"> Studios </h3>
-        <ul className="row genres">
-          {anime.studios.map((studio) => (
-            <li key={uuid()} className="box">{studio.name}</li>
-          ))}
-        </ul>
       </div>
     ) : <LoadingElement />
     )
